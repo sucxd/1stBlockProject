10f347782899af3e1a2e3209791a410742ae1f6c
sucxd <kriss.m03@gmail.com>
2023-10-02 15:39:34 +0200

    working MultipleTag Detection

--- PicoTestSetup-main/Assets/Samples/XR Interaction Toolkit/2.3.2/Starter Assets/Scripts/DetectLayer.cs
+++ PicoTestSetup-main/Assets/Samples/XR Interaction Toolkit/2.3.2/Starter Assets/Scripts/DetectLayer.cs
@@ -2,16 +2,25 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+
 public class DetectLayer : MonoBehaviour
 {
   public string tagToDetect = "Tag1";
+  public string tagToDetect2 = "Tag2";
+  public string tagToDetect3 = "Tag3";
+
+
+public GameObject cube1;
+public GameObject cube2;
+public GameObject cube3;
+ 
 
     private void OnTriggerEnter(Collider other)
     {
-        // Check if the entered object has the specified tag
+        
         if (other.CompareTag(tagToDetect))
         {
-            // Display a message based on the detected tag
+           
             switch (tagToDetect)
             {
                 case "Tag1":
@@ -22,12 +31,32 @@ public class DetectLayer : MonoBehaviour
                     Debug.Log("Tag 2 detected. Do something for Tag 2.");
                     break;
 
-                // Add more cases for other tags as needed
+                case "Tag3":
+                    Debug.Log("Tag 3 detected");
+                    break;
 
                 default:
                     Debug.Log("Unknown tag detected.");
                     break;
             }
+            switch (tagToDetect)
+            {
+                case "Tag1" + "Tag2":
+                    Debug.Log("tag 1 and tag2 detected");
+                    cube1.SetActive(true);
+                    break;
+
+                case "Tag1" + "Tag3":
+                    Debug.Log("Tag1 and Tag3 detected");
+                    cube2.SetActive(true);
+                    break;
+
+                case "Tag2" + "Tag3":
+                    Debug.Log("Tag2 and Tag3 detected");
+                    cube3.SetActive(true);
+                    break;
+            }
         }
+
     }
 }
