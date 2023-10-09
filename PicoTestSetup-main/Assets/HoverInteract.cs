e30318aaddf86af39cf7dc310de6a00891fbd576
sucxd <kriss.m03@gmail.com>
2023-10-09 11:19:31 +0200

    Merge From Kris

--- PicoTestSetup-main/Assets/HoverInteract.cs
+++ PicoTestSetup-main/Assets/HoverInteract.cs
@@ -28,29 +28,4 @@ public class HoverInteract : MonoBehaviour
  
 }
 
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 
-public class HoverInteract : MonoBehaviour
-{
-    public GameObject TextToDisable;
-   void Start()
-   {
-        if(1<2)
-        {
-            Debug.LogFormat("Its WOking");
-        }
-   }
-    public void textPopUp()
-    {
-        if(TextToDisable.activeSelf == true)
-        {
-            TextToDisable.SetActive(false);
-        }else
-        {
-            TextToDisable.SetActive(true);
-        }
-        
-    }
-}
